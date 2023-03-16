014bd20e79214cb34ec7e46e5b1564e3f4bf2538
njdam <jeandamngaboyimana@gmail.com>
2023-03-16 08:05:52 -0700

    A C program that prints sentence, followed by a new line using printf function.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - our function to start a program
+ * printf: as our main function to prints
+ *
+ * Return: our program should retun to value 0 to stop
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
