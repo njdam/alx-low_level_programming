b503074899084d23de475376fd44d9ede8b2588b
njdam <jeandamngaboyimana@gmail.com>
2023-03-16 07:55:47 -0700

    A C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line using puts function.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * puts: as our main function to be used
+ *
+ * function main is used as our starting function to a program
+ * Return: program should end with value 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
