a95566cb1db7df334f4b86e1d2a86f90ce50b4c5
njdam <jeandamngaboyimana@gmail.com>
2023-03-21 06:17:14 -0700

    A unction that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
- * print_alphabert - to check code and start a program
+ * print_alphabet - our function to start a program
  * _putchar: to print lowercase alphabert
  *
  * print_alphabert: our function to print all alphaberts
- * Return: to value 0 for success.
  */
 void print_alphabet(void)
 {
