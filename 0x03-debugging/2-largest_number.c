d948a8e3d1aa30dea9de85a0137fa100d4052f93
njdam <jeandamngaboyimana@gmail.com>
2023-03-22 05:06:14 -0700

    Debugging a program that prints correct the largest of three integers.

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (c > b)
 	{
-		largest = b;
+		largest = c;
 	}
 	else
 	{
-		largest = c;
+		largest = b;
 	}
 
 	return (largest);
