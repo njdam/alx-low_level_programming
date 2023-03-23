5092d47ad262d80a1acb6e7bfab23e3932f5428c
njdam <jeandamngaboyimana@gmail.com>
2023-03-23 14:47:08 -0700

    A program that prints the numbers and Fizz-Buzz.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,25 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (((x % 3) == 0) && ((x % 5) != 0))
+		if (x == 1)
 		{
-			printf("Fizz ");
+			printf("%d", x);
+		}
+		else if (((x % 3) == 0) && ((x % 5) != 0))
+		{
+			printf(" Fizz");
 		}
 		else if (((x % 5) == 0) && ((x % 3) != 0))
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (((x % 3) == 0) && ((x % 5) == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", x);
+			printf(" %d", x);
 		}
 	}
 	printf("\n");
