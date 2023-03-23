16c4bf077aa08985ae03b39751d8a68880a1b236
njdam <jeandamngaboyimana@gmail.com>
2023-03-22 19:30:06 -0700

    A program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,34 +8,45 @@
 int main(void)
 {
 	int v;
-	unsigned long int a = 1, w, x;
-	unsigned long int b = 2, y, z;
-	unsigned long int d = 1000000;
+	unsigned long a = 1, w, x;
+	unsigned long b = 2, y, z;
+	unsigned long d = 1000000;
 
-	printf("%lu, %lu", a, b);
-	for (v = 3; v < 91; v++)
+	printf("%lu, ", a);
+	for (v = 2; v < 92; v++)
 	{
-		b += a;
-		a = b - a;
-		printf(", %lu", b);
+		if (v == 2)
+		{
+			printf("%lu, ", b);
+		}
+		else
+		{
+			b += a;
+			a = b - a;
+			printf("%lu, ", b);
+		}
 	}
-
 	w = (a / d);
 	x = (a % d);
 	y = (b / d);
 	z = (b % d);
-
-	for (v = 91; v < 99; v++)
+	for (v = 92; v < 99; v++)
 	{
 		y = w + y;
 		w = y - w;
 		z = x + z;
 		x = z - x;
-
-		printf(", %lu", (y + (z / d)));
-		printf("%lu", (z % d));
+		if (v == 98)
+		{
+			printf("%lu", (y + (z / d)));
+			printf("%lu", (z % d));
+		}
+		else
+		{
+			printf("%lu", (y + (z / d)));
+			printf("%lu, ", (z % d));
+		}
 	}
-	printf("\n");
-
+	printf(" \n");
 	return (0);
 }
