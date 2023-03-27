ddfe0f89aab7c6e50aaad2de9454b66afca37738
njdam <jeandamngaboyimana@gmail.com>
2023-03-27 14:02:00 -0700

    A program that generates random valid passwords for the program 101-crackme.

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - our function to check code and start a program to generate passwd
+ * putchar: as our function to prints generated password
+ *
+ * Return: to value 0.
+ */
+int main(void)
+{
+	int pswd[100];
+	int pa, pb, pc;
+
+	pa = 0;
+
+	srand(time(NULL));
+
+	for (pb = 0; pb < 100; pb++)
+	{
+		pswd[pb] = rand() % 78;
+		pa += (pswd[pb] + '0');
+		putchar(pswd[pb] + '0');
+		if (((2772 - pa) - '0') < 78)
+		{
+			pc = 2772 - pa - '0';
+			pa += pc;
+			putchar(pc + '0');
+			break;
+		}
+	}
+	return (0);
+}
