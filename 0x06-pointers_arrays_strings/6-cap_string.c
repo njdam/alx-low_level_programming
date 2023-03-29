d38e3240072cc64239add1e42569fe06c5bfa056
njdam <jeandamngaboyimana@gmail.com>
2023-03-29 16:41:54 -0700

    A *cap_string function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
 /**
+ * *cap_string - a function that capitalizes all words of a string;
+ * @str: as our string to be used;
+ *
+ * Return: to resulting string "str".
  */
 char *cap_string(char *str)
 {
@@ -8,7 +12,10 @@ char *cap_string(char *str)
 
 	for (x = 0; str[x] != '\0'; x++)
 	{
-		if (str[x] == 32 || str[x] == 9 || str[x] == '\n' || str[x] == 44 || str[x] == 59 || str[x] == 46 || str[x] == 33 || str[x] == 63 || str[x] == 34 || str[x] == 40 || str[x] == 41 || str[x] == 123 || str[x] == 125)
+		if (str[x] == 32 || str[x] == '\t' || str[x] == '\n' ||
+				str[x] == 44 || str[x] == 59 || str[x] == 46 || str[x] == 33 ||
+				str[x] == 63 || str[x] == 34 || str[x] == 40 || str[x] == 41 ||
+				str[x] == 123 || str[x] == 125)
 		{
 			if (str[x + 1] >= 97 && str[x + 1] <= 122)
 			{
