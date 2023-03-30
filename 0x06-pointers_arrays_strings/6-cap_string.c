7caafd48f1f49a4c82b7e81d29f52d8b0e4596e6
njdam <jeandamngaboyimana@gmail.com>
2023-03-30 05:37:48 -0700

    A *cap_string function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,22 +10,31 @@ char *cap_string(char *str)
 {
 	int x;
 
-	for (x = 0; str[x] != '\0'; x++)
+	x = 0;
+	while (str[x])
 	{
-		if (str[x] == 32 || str[x] == '\t' || str[x] == '\n' ||
-				str[x] == 44 || str[x] == 59 || str[x] == 46 || str[x] == 33 ||
-				str[x] == 63 || str[x] == 34 || str[x] == 40 || str[x] == 41 ||
-				str[x] == 123 || str[x] == 125)
+		while (!(str[x] >= 'a' && str[x] >= 'z'))
 		{
-			if (str[x + 1] >= 97 && str[x + 1] <= 122)
-			{
-				str[x + 1] = str[x + 1] - 32;
-			}
-			else
-				str[x + 1] = str[x + 1];
+			x++;
 		}
-		else
-			str[x] = str[x];
+		if (str[x] == str[0] ||
+				str[x - 1] == 32 ||
+				str[x - 1] == '\t' ||
+				str[x - 1] == '\n' ||
+				str[x - 1] == 44 ||
+				str[x - 1] == 59 ||
+				str[x - 1] == 46 ||
+				str[x - 1] == 33 ||
+				str[x - 1] == 63 ||
+				str[x - 1] == 34 ||
+				str[x - 1] == 40 ||
+				str[x - 1] == 41 ||
+				str[x - 1] == 123 ||
+				str[x - 1] == 125)
+		{
+			str[x] -= 32;
+		}
+		x++;
 	}
 	return (str);
 }
