e92dd0c2f174dfd6d0495403f084755cd7e060e7
njdam <jeandamngaboyimana@gmail.com>
2023-03-30 06:37:27 -0700

    A *cap_string function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,6 +4,9 @@
  * *cap_string - a function that capitalizes all words of a string;
  * @str: as our string to be used;
  *
+ * For line 18, to capitalize starting"!" character"a-z" at
+ * index "x" of each word in string"str".
+ *
  * Return: to resulting string "str".
  */
 char *cap_string(char *str)
@@ -13,14 +16,12 @@ char *cap_string(char *str)
 	for (x = 0; str[x]; x++)
 	{
 		for (; !(str[x] >= 'a' && str[x] <= 'z'); )
-		/* for starting"!" character"a-z" at
-		 * index "x" of each word in string"str". */
 		{
 			x++;
 		}
 		if (x == 0 ||
 				str[x - 1] == 32 ||
-				str[x - 1] == 9 ||
+				str[x - 1] == '\t' ||
 				str[x - 1] == 10 ||
 				str[x - 1] == 44 ||
 				str[x - 1] == 59 ||
