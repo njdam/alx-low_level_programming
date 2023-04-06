cb1931dc90032ba6a64b09b17feaa7c2c706a70e
njdam <jeandamngaboyimana@gmail.com>
2023-04-06 06:32:14 -0700

    A *infinite_add function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,22 +11,10 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int x, y;
-	char *r[size_r];
-	char res;
+	r[size_r] = *n1 + *n2;
 
-	r = &res;
-	for (x = 0; *(n1 + x) != '\0';)
-		x++;
-	for (y = 0; *(n2 + y) != '\0';)
-		y++;
-	x--;
-	y--;
-	if (x >= size_r || y >= size_r)
-		return (0);
-	else
-	{
-		res = res + (n1[x] + n2[y]);
-	}
-	return (res);
+	if (/*n1 != 0 && *n2 != 0 && */*r >= 48)
+		return (r);
+
+	return (0);
 }
