caebd10363f2f842826a9fa844ea379f843934da
njdam <jeandamngaboyimana@gmail.com>
2023-04-07 07:23:16 -0700

    A *infinite_add function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -21,33 +21,35 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	n2ln = 0; /* finding of length of String n2 */
 	while (n2[n2ln] != '\0')
 		n2ln++;
-	if (n1ln >= n2ln && n1ln < (size_r - 1))
+	if (n1ln >= n2ln && n1ln < (size_r - 1) && n2ln > 0)
 	{
 		for (x = n1ln - 1; x >= 0; x--)
 		{
 			/* conversion of strings to numbers */
-			res = (n1[x] - '0') + (n2[x] - '0') + res;
+			res = (n1[x] - '0') + (n2[n2ln - 1] - '0') + res;
 			/* store the remender in buffer */
 			r[size_r] = ((res % 10) + '0');
 			/* this will be added to the next res */
 			res = res / 10;
 			/* buffer size decrementation to next */
 			size_r--;
+			n2ln--;
 			break;
 		}
 	}
-	else if (n1ln < n2ln && n2ln < (size_r - 1))
+	else if (n1ln < n2ln && n2ln < (size_r - 1) && n1ln > 0)
 	{
 		for (x = n2ln - 1; x >= 0; x--)
 		{
 			/* conversion of strings to numbers */
-			res = (n1[x] - '0') + (n2[x] - '0') + res;
+			res = (n1[n1ln - 1] - '0') + (n2[x] - '0') + res;
 			/* store the remender in buffer */
 			r[size_r] = ((res % 10) + '0');
 			/* this will be added to the next res */
 			res = res / 10;
 			/* buffer size decrementation to next */
 			size_r--;
+			n1ln--;
 			break;
 		}
 	}
