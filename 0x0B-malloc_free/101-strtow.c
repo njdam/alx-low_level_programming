e5e554c763f297660f1394cef980f0dc42c7248c
njdam <jeandamngaboyimana@gmail.com>
2023-04-12 04:58:46 -0700

    A strtow function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,65 +8,70 @@
  */
 char **strtow(char *str)
 {
-	int x, i, y, j, z, ln;
-	char **w;
+	int ln, y, x, i, z, a, j;
+	char **w, *m;
 
-	if (str == NULL || str == 0)
-		return (NULL);
-	y = x = ln = 0;
+	ln = 0;
 	while (str[ln])
-	{
-		if (str[ln] != ' ')
-		{
-			for (x = 1; str[ln] != ' ' && str[ln] != '\0';)
-				x++;
-			for(y = 1; str[ln + 1] == ' ' || str[ln + 1] == '\0';)
-				y++;
-		}
 		ln++;
-	}
+	y = stwct(str);
+	if (y == 0)
+		return (NULL);
 
-	w = malloc((ln + 1) * sizeof(char));
+	w = malloc((y + 1) * sizeof(char *));
 	if (w == NULL)
 		return (NULL);
-	for (z = 0; z < ln; z++)
+	i = 0;
+	j = 0;
+	for (x = 0; x <= ln; x++)
 	{
-	for (j = 0; j <= y; j++)
-	{
-		i = 0;
-		while (str[z] != ' ' || str[z] != '\0')
+		if (str[x] == ' ' || str[x] == '\0')
 		{
-			w[j][i] = str[z];
-			i++;
+			if (i)
+			{
+				z = x;
+				m = malloc((i + 1) * sizeof(char));
+				if (m == NULL)
+					return (NULL);
+				while (a < z)
+					*m++ = str[a++];
+				*m = '\0';
+				w[j] = m - i;
+				j++;
+				i = 0;
+			}
 		}
-		i = 0;
-		while ((str[z] == ' ' && str[z + 1] != ' ') || str[z] != '\0')
-		{
-			w[j][i] = '\0';
-			i++;
-		}
+		else if (i++ == 0)
+			a = x;
 	}
-	}
-	w[j][i] = '\0';
+	w[j] = NULL;
+
 	return (w);
 }
-/*
-int strct(char *st)
-{
-	int x, y, i;
 
-	y = x = i = 0;
-        while (str[i])
+/**
+ * stwct - my function to count the number of character in a word;
+ * @st: our string with words to be used;
+ *
+ * Return: the resulting number of characters in a word.
+ */
+int stwct(char *st)
+{
+	int l, d, n;
+
+	d = 0;
+	l = 0;
+	n = 0;
+        while (st[n] != '\0')
         {
-                if (str[i] != " ")
-                {
-                        x += 1;
-                        if (str[i + 1] == ' ' && str[i + 1] != '\0')
-                                return (x);
-                                y += 1;
-                }
-                i++;
+                if (st[n] == ' ')
+                        d = 0;
+		else if (d == 0)
+		{
+			d = 1;
+			l++;
+		}
+                n++;
         }
-        return (y);
+        return (l);
 }
-*/
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -11,5 +11,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+int stwct(char *st);
 
 #endif /* MAIN_H */
