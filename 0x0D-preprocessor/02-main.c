3afee1371924917ffaf0d6b065e2cfb72d36a81c
njdam <jeandamngaboyimana@gmail.com>
2023-04-17 12:28:41 -0700

    A program that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - functions that prints name of c-file(file of source code);
+ *
+ * Return: value 0 for sucess.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#define MAIN_H
+
+#if x < 0
+#define ABS(x) (x * -1)
+
+#endif /* Function Like Macro */
