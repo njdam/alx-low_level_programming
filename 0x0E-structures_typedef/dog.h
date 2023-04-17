ba6a3dc7579a82d955393422ab712aa05b6dfa90
njdam <jeandamngaboyimana@gmail.com>
2023-04-17 15:27:44 -0700

    A defining a new type of "struct dog".

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,6 +4,9 @@
 /**
  * struct dog - function containing dog information;
  * my_dog - typedef for new dog struct dog;
+ * @name: our First member;
+ * @float: our Second member;
+ * @owner:  our Third member;
  */
 
 typedef struct dog
