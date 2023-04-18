e29909ba43fa2139d9f50f58a4030c1ff63169e3
njdam <jeandamngaboyimana@gmail.com>
2023-04-17 18:01:53 -0700

    A free_dog function that frees dogs.

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 
-/* free_dog - a function to free allocated "dogs" memory;
+/**
+ * free_dog - a function to free allocated "dogs" memory;
  * @d: pointing to struct dog to be free;
  *
  * Return: value is nothing.
