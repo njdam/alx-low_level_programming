1712135f84bd717f37441c709e84def184c9a6ee
njdam <jeandamngaboyimana@gmail.com>
2023-04-19 15:56:40 -0700

    A program to perform simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
-	char *rdmas;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -19,22 +19,22 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	rdmas = argv[2];
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (rdmas[1] != '\0' || get_op_func(rdmas) == NULL)
+	if (op[1] != '\0' || get_op_func(op) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*rdmas == '%' && num2 == 0) || (*rdmas == '/' && num2 == 0))
+	if ((*op == '%' && num2 == 0) || (*op == '/' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(rdmas)(num1, num2));
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }
