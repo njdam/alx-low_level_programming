3ea6119c7712e1ce5f3feca90571d93e7c30a169
njdam <jeandamngaboyimana@gmail.com>
2023-05-08 10:50:08 -0700

    A function to create a file and insert content in it.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,11 +16,16 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	/* `S_IRUSR | S_IWUSR` or `0600` in octal notation */
+
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[strln])
-		strln++;
+	if (text_content != NULL)
+	{
+		strln = 0;
+		while (text_content[strln])
+			strln++;
+	}
 
 	wr = write(fd, text_content, strln);
 	if (wr == -1)
