5423f8199a666edb53e894881045111064639b34
njdam <jeandamngaboyimana@gmail.com>
2023-05-08 14:53:01 -0700

    A function to copy a content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-#define BUFSZ 1024
 /**
  * main - a function to copy content from one file to another;
  * @argc: numbers of of arguments on program command line;
@@ -19,31 +18,31 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = malloc(BUFSZ * sizeof(char));
+	buffer = malloc(1024 * sizeof(char));
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	src = open(argv[1], O_RDONLY);
-	rd = read(src, buffer, BUFSZ);
+	rd = read(src, buffer, 1024);
 	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	do {
-	if (src == -1 || rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		free(buffer);
-		exit(98);
-	}
-	wr = write(dest, buffer, BUFSZ);
-	if (dest == -1 || wr == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		free(buffer);
-		exit(99);
-	}
-	rd = read(src, buffer, BUFSZ);
-	dest = open(argv[2], O_WRONLY | O_APPEND);
+		if (src == -1 || rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+		wr = write(dest, buffer, 1024);
+		if (dest == -1 || wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		rd = read(src, buffer, 1024);
+		dest = open(argv[2], O_WRONLY | O_APPEND);
 	} while (rd > 0);
 	free(buffer);
 	fl_close(src);
@@ -60,8 +59,9 @@ int main(int argc, char *argv[])
  */
 void fl_close(int fd)
 {
-	int cl = close(fd);
+	int cl;
 
+	cl = close(fd);
 	if (cl == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
