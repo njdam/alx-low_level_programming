dd9c75b47154fd1beb41ba1bc6647cf3f3922b3b
njdam <jeandamngaboyimana@gmail.com>
2023-06-15 15:53:57 +0000

    A function to insert new node at a given index in doubly linked list.

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,65 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - is a functiom to add new node at index in a list;
+ * @h: is a head node of doubly linked list;
+ * @idx: is a given index where to add new node in dlistint_t list;
+ * @n: is element of a new node to be added at index in dlistint_t list;
+ *
+ * Return: address of new node added otherwise NULL.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0, nlen;
+	dlistint_t *temp, *current = *h;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	nlen = dlistint_len(*h);
+	if (idx > nlen)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->n = n;
+	if (nlen == idx)
+	{
+		new_node = add_dnodeint_end(&(*h), n);
+		return (new_node);
+	}
+
+	while (i < (idx - 1) && current->next)
+	{
+		i++;
+		current = current->next;
+	}
+
+	temp = current->next;
+	current->next = new_node;
+	new_node->prev = current;
+	new_node->next = temp;
+
+	return (new_node);
+}
+
+/**
+ * dlistint_len - a func to return number of elements in doubly list;
+ * @h: is a head node of doubly linked list;
+ *
+ * Return: value equal to a number of elements in doubly linked list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	ssize_t n_node = 0;
+
+	while (h)
+	{
+		n_node++;
+		h = h->next;
+	}
+
+	return (n_node);
+}
