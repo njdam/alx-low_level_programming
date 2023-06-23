5c3312ec2f52e64caa4a463bed596d1e2c024819
njdam <jeandamngaboyimana@gmail.com>
2023-06-23 08:06:03 +0000

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/njdam/alx-low_level_programming.git/0x18-dynamic_libraries/libmk_me_win.so
-export LD_PRELOAD=/$PWD/libmk_me_win.so ldd ./mg
+export LD_PRELOAD=/$PWD/libmk_me_win.so
+./gm 9 8 10 24 75 9
