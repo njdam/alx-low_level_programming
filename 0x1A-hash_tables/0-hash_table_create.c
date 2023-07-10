cddf60c110282bd18d37e47ab98714e891100ac0
njdam <jeandamngaboyimana@gmail.com>
2023-07-10 22:36:13 +0000

    A function that creates a hash table.

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - is a function to create a hash table;
+ * @size: is size of hash table;
+ *
+ * Return: address of created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!(ht->array))
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		ht->array[i] = NULL;
+		i++;
+	}
+	ht->size = size;
+
+	return (ht);
+}
