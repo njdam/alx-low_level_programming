1009e65528c84c17e714f8f404bd3d7970d1bdcf
njdam <jeandamngaboyimana@gmail.com>
2023-07-11 12:04:39 +0000

    A function to retrieves a value associated with a key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,14 +13,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *current;
 
+	if (!ht || !*key || !key)
+		return (NULL);
+
 	index = hash_djb2((unsigned char *)key) % ht->size;
-	if (index >= ht->size)
+	if (index >= ht->size )
 		return (NULL);
 
 	current = ht->array[index]; /* Assigning head node at index */
-	if (current == NULL)
-		return (NULL);
-
 	while (current)
 	{
 		if (strcmp(current->key, key) == 0)
